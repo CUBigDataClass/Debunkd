3b79392ba6611ad73294ed87fac01901b88c61e0
Karthik Kannan <kknicks@gmail.com>
2017-03-08 18:21:56 -0700

    Create snopes.py to get results of news from snopes

---
+++ snopes.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import string
+def snopes_scrape(search_term, num= 5):
+
+	"""
+	Returns the top <num< matches from Snopes for a given keyword
+	Args:
+		search_term(string) : The search term that is queried on Snopes.
+		num(int) L: Number of matches, maximum 18 for now. 
+
+	Returns:
+		list: The top num matches from Snopes.
+	"""
+	if num>18:
+		raise ValueError("The maximum value of number of results can be 18.")
+
+    search_term = search_term.replace(" ", "+")
+    r= requests.get('http://www.snopes.com/?s='+search_term+"&category=facts")
+    soup = BeautifulSoup(r.text, 'lxml') 
+    matches= [i.text for i in soup.find_all("h2", 
+    			{"class":'article-link-title'})[:num]]
+    punctmap = str.maketrans('', '', string.punctuation+'‘’')
+    matches = [s.translate(punctmap) for s in matches]
+    return matches
+
+if __name__== "__main__":
+    s = input("Enter search term: ")
+    res = snopes_scrape(s)
+    print (res)
