7743597ca692eb36507b5f465cec81604aca596c
Karthik Kannan <kknicks@gmail.com>
2017-04-08 01:13:43 -0600

    Kafka integrations added

--- debunkr/gnip/gnipreader.py
+++ debunkr/gnip/gnipreader.py
@@ -1,13 +1,28 @@
 import requests
 import configparser
+import json
+from kafka import KafkaProducer
 
+#We need better storage for all of these
+#----------------------------------------
 config = configparser.ConfigParser()
 config.read('credentials.ini')
 api_user = config['GNIP_API']['username']
 api_pass = config['GNIP_API']['password']
+TOPIC_NAME= "gnipstream"
+KAFKA_ADDRESS = "localhost:9092"
+#----------------------------------------
+
 
 class GnipData():
-    
+    """
+    Gets data from Gnip and pushes it to a Kafka queue. 
+    Params-
+    - maxResults (int): How many Results you want
+    - maxResultsPerPage (int): Results per page, to get further we use the next param
+    - fromDate (string- yyyymmddhhmmss) : Starting Date of all the tweets returned
+    - toDate (string -yyyymmddhhmmss): Ending Date of all the tweets returned
+    """
     def __init__(self, maxResults, maxResultsPerPage, fromDate, toDate):
         self.maxResults = maxResults
         self.fromDate = fromDate
@@ -15,8 +30,20 @@ class GnipData():
         self.url = "https://gnip-api.twitter.com/search/" \
             "fullarchive/accounts/greg-students/prod.json"
         self.maxResultsPerPage = maxResultsPerPage
-    
+        self.kafka_server = KAFKA_ADDRESS
+        #this works in jupyter but not in terminal... working around
+        # self.kafka_producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('unicode'),
+        #                                    bootstrap_servers=[self.kafka_server])
+        self.kafka_producer = KafkaProducer(bootstrap_servers = [self.kafka_server])
+        
     def fetchTweets(self, query):
+        """
+        Takes a query and pushes the relevant tweets to Kafka
+        Params
+        - Query: Search term for Gnip
+
+        Returns : Nothing
+        """
         extended_query = query+" has:geo place_country:us"
         params = {'query':extended_query, 
                   'maxResults':self.maxResultsPerPage,
@@ -26,25 +53,31 @@ class GnipData():
         response = requests.get(self.url, params=params, \
                          auth=(api_user, api_pass))
 
-        self.queueKafka(response.json()['results'],-1 )
+        self.queueKafka( json.dumps(response.json()['results']).encode('utf-8'))
 
-        for i in range(int(self.maxResults/self.maxResultsPerPage)-1):
+        #Scrolling through until next runs out or maxResults is exceeded
+        for i in range(int(self.maxResults/self.maxResultsPerPage)):
             if 'next' in response.json().keys():
                 params= {'query':extended_query, "next": response.json()['next']}
                 response = requests.get(self.url, params=params, \
                              auth=(api_user, api_pass))
-                self.queueKafka(response.json()['results'],i)
+                self.queueKafka(json.dumps(response.json()['results']).encode('utf-8'))
             else:
                 break
 
     
-    def queueKafka(self, json_data, i ):
-        # queue json data in kafka
-        print(i) #Remove this and the paremeter later.
+    def queueKafka(self, json_data ):
+        """
+        Queues json to Kafka
+        Params
+        - json_data: Json data to queue in Kafka
 
+        Returns : Nothing
+        """
+        self.kafka_producer.send(TOPIC_NAME, json_data)
 
 
 if __name__ == "__main__":
-    a = GnipData(100, 10 , "201612300000", "201612310000")
+    a = GnipData(11, 10 , "201612300000", "201612310000")
     a.fetchTweets("Apple")
 
