d0fa27076d63e54b324b7978dac87b958902315c
Karthik Kannan <kknicks@gmail.com>
2017-04-23 18:46:01 -0600

    Fixed typo

--- debunkr/gnip/gnipreader.py
+++ debunkr/gnip/gnipreader.py
@@ -61,7 +61,7 @@ class GnipData():
                          auth=(api_user, api_passwd))
 
         for r in response.json()['results']:
-                r['topic']= Query
+                r['topic']= query
                 self.queueKafka( json.dumps(r).encode('utf-8'))
 
         #Scrolling through until next runs out or maxResults is exceeded
