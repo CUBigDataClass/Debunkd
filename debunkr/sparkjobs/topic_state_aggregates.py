1cb901be2cf22ba265cc5936b07c980b4ea4e39b
kepai91 <keerthipai91@gmail.com>
2017-04-29 16:25:28 -0600

    State aggregate script, read and writes into prod-cassandra

--- debunkr/sparkjobs/topic_state_aggregates.py
+++ debunkr/sparkjobs/topic_state_aggregates.py
@@ -12,13 +12,14 @@ from pyspark.sql.functions import count
 
 if __name__ == "__main__":
     ##Create a spark session with cassandra
-    spark = SparkSession.builder.appName("SparkCassandraApp").config("spark.cassandra.connection.host", "172.32.13.183").config("spark.cassandra.connection.port", "9042").master("local[2]").getOrCreate();
+    spark = SparkSession.builder.appName("SparkCassandraApp").config("spark.cassandra.connection.host", "sb-k8s-ingress-1525602578.us-west-2.elb.amazonaws.com").config("spark.cassandra.connection.port", "9042").master("local[2]").getOrCreate();
 
     ##Load the table as a dataframe
     df = spark.read.format("org.apache.spark.sql.cassandra").options(table="tweets_master", keyspace="swashbucklers").load();
     ##df.show()
 
     ##aggregate over various states for a given topic
-    topics_count = df.select("topic","location").groupBy("topic", "location").agg(count("location").alias("count")).filter("location is not null" or "location" != '')
+    topics_state_count = df.select("topic","location").groupBy("topic", "location").agg(count("location").alias("count")).filter("location is not null" or "location" != '')
+    topics_state_count.write.format("org.apache.spark.sql.cassandra").options(table="state_aggregates", keyspace="swashbucklers").save(mode="overwrite")
     topics_state_count.unpersist()
     ##topics_count.show()
